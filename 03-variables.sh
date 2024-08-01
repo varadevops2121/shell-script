97c5708f294dfee2fb21fc50bb701aae9b150238
varalaxmi <varadevops2121@gmail.com>
2024-08-01 17:08:04 +0530

    describe variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,10 +3,12 @@
 
 #declaring of variable
 PERSON1=Sachin  #no space between = and value
-PERSION2=Suresh
+PERSION2=Rahul
 
 #referring variable
 echo "$PERSON1:: Hello $PERSON2, How are you?"
-echo "${PERSON2}:: Hi $PERSON1, I am fine. How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine. How are you?"
 echo "$PERSON1:: I am fine too. how is your work?"
 echo "$PERSON2:: not bad. I am thinking to upgrade to DevOps"
+
+
