d07bd6d543d3e588c8fd635e67959bd66aa0305b
varalaxmi <varadevops2121@gmail.com>
2024-08-01 17:54:58 +0530

    describe variables

---
+++ 05-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Please enter username::"
+
+read USERNAME #here USERNAME is variable 
+
+echo "Please Enter Password::"
+
+read PASSWORD
+
+echo "Username is: $USERNAME, Password is: $password"
