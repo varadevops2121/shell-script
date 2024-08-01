6fb1265d613ae30f9f84e5c4e1d36aa0e83d5e87
varalaxmi <varadevops2121@gmail.com>
2024-08-01 16:49:41 +0530

    hello script

---
+++ 01-hello-world.sh
@@ -0,0 +1,2 @@
+#!/bin/bash 
+echo "hello iam practing shell-script"
---
+++ 03-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+#declaring of variable
+PERSON1=Sachin  #no space between = and value
+PERSION2=Suresh
+
+#referring variable
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "${PERSON2}:: Hi $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: I am fine too. how is your work?"
+echo "$PERSON2:: not bad. I am thinking to upgrade to DevOps"
